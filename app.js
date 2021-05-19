8f11eee7a9ea02a6abc0af0405538f9c55c942d1
qiluosheng <crazy_zy01@163.com>
2021-05-19 16:05:41 +0800

    小程序生命周期

---
+++ app.js
@@ -0,0 +1,40 @@
+App({
+
+  // 生命周期函数   后台存活两个小时
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    console.log("小程序初始化完成了：function"),
+    //异步调用
+    wx.getUserInfo({
+      // 数据拿到之前，在回调
+      success: function(res){
+        console.log(res)
+      }
+      
+    })
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    console.log("界面显示出来 onShow")
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    console.log("界面被隐藏时候出发：onHide")
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    console.log("当小程序发生错误时候执行：onError")
+  }
+})
